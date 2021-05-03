cc3ffb69c60fd1cb214ded9048e0676c6d9910b4
CamiloFuentesR <lokomilo1@gmail.com>
2021-05-03 12:01:58 -0400

    ASD

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const cors = require('cors');
 
 
 const app = express();
+const whiteList = ['https://best-sale.netlify.app/home']; //hace accesible solo desde esta url acccion
+const corsOptions = {
+    origin: (origin, callbaback) => {
+        //console.log(origin);
+        const existe = whiteList.some(dominio => dominio === origin);
+        if (existe) {
+            callbaback(null, true)
+        } else {
+            callbaback(new Error('No permitido por cors'))
+        }
+    }
+}
 
 //db conexion
 db.authenticate()
@@ -12,7 +24,7 @@ db.authenticate()
     .catch(error => console.log(error));
 // db.sync({force:false});
     app.use(express.json());
-// app.use(cors())
+    app.use(cors(corsOptions))
     // app.use(express.static('public'));
 
     app.use('/api/user', require('./routes/userRouter'));
