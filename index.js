0420a8b02abf3331c0b36cb709e390a1bb383243
CamiloFuentesR <lokomilo1@gmail.com>
2021-05-03 10:51:55 -0400

    asdsa

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const app = express();
 
 //db conexion
 db.authenticate()
-    .then(()=> console.log('bd on line'))
+    .then(() => console.log('bd on line'))
     .catch(error => console.log(error));
 // db.sync({force:false});
-    app.use(express.json());
-// app.use(cors())
-    // app.use(express.static('public'));
+app.use(express.json());
+app.use(cors())
+// app.use(express.static('public'));
 
-    app.use('/api/user', require('./routes/userRouter'));
-    app.use('/api/sales',require('./routes/salesRouter'));
-    app.use('/api/auth',require('./routes/authRouter'));
+app.use('/api/user', require('./routes/userRouter'));
+app.use('/api/sales', require('./routes/salesRouter'));
+app.use('/api/auth', require('./routes/authRouter'));
 
-    const port = process.env.PORT || 4000;
-    const host = process.env.HOST || '0.0.0.0.'
-    app.use(express.urlencoded({extended:true}));
+const port = process.env.PORT || 4000;
+const host = process.env.HOST || '0.0.0.0.'
+app.use(express.urlencoded({ extended: true }));
 
-    app.listen(port, host, () => {
-        console.log(`Server run in port ${port}`)
-    })
+app.listen(port, host, () => {
+    console.log(`Server run in port ${port}`)
+})
